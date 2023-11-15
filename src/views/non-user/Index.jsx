cbc1a90f5e8cde121e376ab882029e322e98bd71
Thana <Thana.devproj@gmail.com>
2023-11-15 18:38:18 +0700

    bg after logo

--- src/views/non-user/Index.jsx
+++ src/views/non-user/Index.jsx
@@ -3,12 +3,23 @@ import React from "react";
 function Index() {
   return (
     <>
-      <div>
-        <div className="w-[100vw] h-[100vh] flex flex-column justify-center mt-[300px] ">
-          <img
-            src="src\images\Path-of-Exile-Logo.png"
-            className="h-[30vh] animate-floating"
-          />
+      <div className="">
+        <div
+          className="bg-cover bg-center relative"
+          style={{
+            backgroundImage: `url('src/images/cover_1.jpg')`,
+            height: "100vh",
+          }}
+        >
+          <div className="flex flex-column justify-center items-center h-full">
+            <img
+              src="src/images/Path-of-Exile-Logo.png"
+              className="h-[30vh] animate-floating"
+              alt="Logo"
+            />
+          </div>
+
+          <div className="h-[100dvh]"></div>
         </div>
       </div>
     </>
